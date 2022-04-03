77f092807bef8418fa4600eac33f4b3a65cd6c66
snowleopardAlexa <alexaslomski@gmail.com>
2022-04-02 20:59:39 -0400

    add footer styles

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,24 +1,28 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { AppBar, Toolbar, Typography, Container } from "@material-ui/core"
+import useStyles from '../utils/styles'
 
 const Layout = ({ children }) => {
+
+  const classes = useStyles()  
+
   return (
     <>
       <Head>
         <title>Apple Store</title>
       </Head>
-      <AppBar position="static">
+      <AppBar position="static" className={classes.navbar}>
         <Toolbar>
           <Typography>
              <Image src="/apple.svg" width="40px" height="40px" alt="apple-logo" />
           </Typography>
         </Toolbar>
       </AppBar>
-      <Container>
+      <Container className={classes.main}>
           {children}
       </Container>
-      <footer>
+      <footer className={classes.footer}>
           <Typography>
               All rights reserved to Apple Store &copy;
           </Typography>
---
+++ utils/styles.js
@@ -0,0 +1,22 @@
+// Material-UI styles
+import { makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles({
+    navbar: {
+        backgroundColor: '#F2F2F2',
+        '& a' : {
+            color: '#000',
+            marginLeft: 10,
+        }
+    },
+    main: {
+        minHeight: '80vh'
+    },
+    footer: {
+        backgroundColor: '#F2F2F2',
+        textAlign: 'center',
+        padding: '20px'
+    }
+})
+
+export default useStyles
