8bc86e1ed6193a23d95149a027db53b83a5b6c40
snowleopardAlexa <alexaslomski@gmail.com>
2022-04-02 21:02:30 -0400

    remove an issue with material ui server side rendering

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,15 @@
+import { useEffect } from 'react'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+
+  // fix material-ui no styles changed on refreshing the web
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles)
+    }
+  }, [])
   return <Component {...pageProps} />
 }
 
